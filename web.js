2cf8a7d241b2eb103448bde64d0a84c3aa375368
Ubuntu <ubuntu@ip-172-31-44-115.us-west-2.compute.internal>
2013-07-08 18:23:45 +0000

    added myvalue

--- web.js
+++ web.js
@@ -2,6 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var myvalue = '';
+fs.readFile("index.html", "utf-8", functioin(error, data){
+myvalue = data;
+});
+
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
