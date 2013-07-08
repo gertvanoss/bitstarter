669ceebf6d5538c28567b8308f263831c7b9ba8b
Gert van Oss <gertvanoss@me.com>
2013-07-08 20:16:51 +0000

    it works

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-var myvalue = new Buffer(25);
- response.send(fs.readFileSync("index.html"),myvalue.toString());
+  var data = new Buffer(fs.readFileSync("index.html","utf-8"));
+  var myvalue = data.toString('utf-8');
+  response.send(myvalue);
+//console.log(myvalue);
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
