04b7a9435c5226aa7a2a5eaeecda911dde1ecf44
Gert van Oss <gertvanoss@me.com>
2013-07-08 19:20:59 +0000

    closed parenthesis

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var myvalue = new Buffer(25);
- response.send(fs.readFileSync("index.html"),myvalue.toString();
+ response.send(fs.readFileSync("index.html"),myvalue.toString());
 });
 
 var port = process.env.PORT || 5000;
