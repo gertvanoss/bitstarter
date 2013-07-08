626f7233363d6fd0f0e47bfb30b12332cfab08c3
Gert van Oss <gertvanoss@me.com>
2013-07-08 18:32:05 +0000

    function changed

--- web.js
+++ web.js
@@ -3,13 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var myvalue = '';
-fs.readFile("index.html", "utf-8", functioin(error, data){
+fs.readFile("index.html", "utf-8", function(error, data){
 myvalue = data;
 });
 
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(myvalue);
 });
 
 var port = process.env.PORT || 5000;
